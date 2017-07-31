3db62f6be7a259bcefa671edf694f03c7b1ea473
Alfrex <alfredo.morote@ucsp.edu.pe>
2017-07-31 08:42:18 -0500

    upload files

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,88 +1,87 @@
-function init () {
-
-    // Setup Google Map
-    var map = new google.maps.Map(document.getElementsByClassName('map-filter')[0], {
-        zoom: 5,
-        center: { lat: -16.344812, lng: -71.56799 }
-    });
-
-    // Load closest airport
-    var loadClosestAirport = function (pos, callback) {
-        $.get('/api/closest-airport?lat='+pos.lat+'&lng='+pos.lng, function (body) {
-            callback(body);
-        });
-    }
-
-    // Load flights to airports
-    var flightsToAirports = function (id1, id2, callback){
-        $.get('/api/flights?id1='+id1+'&id2='+id2, function (body) {
-            callback(body);
-        });
-    }
-
-    // Setup markers
-    var markers = {};
-    var airports = {};
-    markers.a = new google.maps.Marker({
+function create_mark(mark,map,_lat, _long,img){
+    var marker = new google.maps.Marker({
         label: 'A',
         map: map,
         draggable: true,
         animation: google.maps.Animation.DROP,
-        position: { lat: -16.344812, lng: -71.56799 }
+        icon: img,
+        position: { lat: _lat, lng: _long }
     });
-    markers.b = new google.maps.Marker({
-        label: 'B',
-        map: map,
-        draggable: true,
-        animation: google.maps.Animation.DROP,
-        position: { lat: -16.344812, lng: -71.56799 }
+    mark.push(marker);
+}
+function init(){
+
+    var markers = [];
+    var tam = 0;
+    var image = '/images/';
+    var trees = ['arbol1.png', 'arbol2.png','arbol3.png','arbol4.png','arbol5.png','arbol6.png','arbol7.png','arbol8.png','arbol9.png','arbol10.png','arbol11.png','arbol12.png','arbol13.png','arbol14.png','arbol15.png','arbol16.png','arbol17.png','arbol18.png','arbol19.png','arbol20.png','arbol21.png','arbol22.png','arbol23.png','arbol24.png','arbol25.png','arbol26.png'];
+    var cuenta = 0;
+    var nro = $('#arbol').val();
+    //var c = document.URL;
+    //document.getElementById('arbol').value = nro;
+
+    //console.log(c);
+    var img_tree = image+trees[(parseInt(nro)%26)];
+    console.log(img_tree);
+
+    var map = new google.maps.Map(document.getElementsByClassName('map-filter')[0], {
+        zoom: 13,
+        center: { lat: -16.344812, lng: -71.56799 }
     });
 
-    // Setup markers listeners
-    markers.a.addListener('dragend', function () {
-        loadClosestAirport({
-            lat: markers.a.getPosition().lat(),
-            lng: markers.a.getPosition().lng()
-        }, function (airport) {
-            console.log(airport);
-            airports.a = airport;
-            if(airports.a && airports.b) {
-                flightsToAirports(airports.a.id, airports.b.id, function(flights) {
-                    console.log(flights);
-                    var my_table = document.createElement("table");
-                    var container = document.getElementsByClassName("results-container")[0];
-                    container.innerHTML = "";
-                    container.appendChild(my_table);
-                    for(var i = 0; i < flights.length; i++) {
-                        var my_tr = document.createElement("tr");
-                        var my_td = document.createElement("td");
-                        my_td.innerHTML = flights[i].name;
-                        my_tr.appendChild(my_td);
-                        my_table.appendChild(my_tr);
-                    }
-                });
-            }
-        })
-    })
-    markers.b.addListener('dragend', function () {
-        loadClosestAirport({
-            lat: markers.b.getPosition().lat(),
-            lng: markers.b.getPosition().lng()
-        }, function (airport) {
-            console.log(airport);
-            airports.b = airport;
-            if(airports.a && airports.b) {
-                flightsToAirports(airports.a.id, airports.b.id, function(flights) {
-                    console.log(flights);
-                });
-            }
-        })
-    })
+    
+    var poss = 2;
+    var text;
+    var loadInfo = function (pos,callback) {
+        $.get('/api/clo?lat=11', function (body) {
+            callback(body);
+        });
+    }
 
-    /*var my_table = document.createElement("table");
-    var container = document.getElementsByClassName("results-container")[0];
-*/
-//    container.appendChild(my_table);
-
+    //create_mark(markers,map,-16.344812,-71.56799);
+    
+    
+    var tmp = loadInfo(poss,function(rell){
+        //console.log(rell);
+        text = rell.split("\n");
+        tam = text.length;
+        console.log("tama: "+tam);
+        //console.log(text);
+        console.log(text.length);
+        for (var i = 0; i < tam; i++){
+            text[i] = text[i].split(",");
+        }
+        /*console.log(text[0],map.center);
+        map.center.lat = parseFloat(text[0][1]);
+        map.center.lng = parseFloat(text[0][2]);*/
+        
+        var my_lat = 0;
+        var my_lng = 0;
+        for(var i = 0; i < tam; i = i + 20){
+            if(text[i][3] == nro){
+                my_lat = parseFloat(text[i][1]);
+                my_lng = parseFloat(text[i][2]);
+                cuenta++;
+                create_mark(markers,map,parseFloat(text[i][1]),parseFloat(text[i][2]),img_tree);
+            }
+            
+        }
+        /*var marker_tmp = new google.maps.Marker({
+            label: 'A',
+            map: map,
+            draggable: true,
+            animation: google.maps.Animation.DROP,
+            position: { lat: my_lat, lng: my_lng }
+        });
+        console.log(marker_tmp.getPosition());*/
+        map.setCenter({lat: my_lat, lng: my_lng}); 
+        console.log("Cuenta: " + cuenta);
+        console.log(259%26);
+        console.log(2%26);
+        console.log(5%26);
+        //console.log(typeof(text[0][1]));
+        //console.log(text);
+        //text = rell;
+    });
 
 }
--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 // Import dependencies
 var express = require('express');
 var path = require('path');
-var pg = require('pg');
+//var pg = require('pg');
+
+var fs = require('fs');
+
+var info;
+
+fs.readFile('./public/files/result.csv', 'utf8', function(error, data){
+  info = data;
+    //console.log(data);
+});
 
 
 //Setup DB credentials
-var client = new pg.Client({
+/*var client = new pg.Client({
       user: 'postgres',
       password: 'postgres',
       database: 'airport',
       host: 'localhost',
       port: 5432
     });
-
+*/
 //conect to postgrest
-//client.on('drain', client.end.bind(client));
-client.connect();
+//client.connect();
 
 
 // Initialize server
@@ -25,11 +33,13 @@ var server = express();
 server.use('/', express.static(__dirname+'/public'));
 
   
-//var airportQuery = client.query("select distinct a.name, a.Country from flights as a inner join sourceStation as ss on a.source_airport_id=ss.id inner join destinationStation as ds on a.destination_airport_id = ds.id where ss.id=2805 and ds.id=6073");
-
+server.get('/api/clo', function(req,res){
+  res.send(info);
+  res.end();
+})
 
 // Setup routes
-server.get('/api/closest-airport', function (req, res) {
+/*server.get('/api/closest-airport', function (req, res) {
     client.query("select a.id, a.name, a.country, a.city, ST_Distance(ST_GeomFromText('POINTZ( "+req.query.lat+" "+req.query.lng+" 2335 )' ), a.location) as distance from station as a where ST_DWithin( ST_GeomFromText('POINTZ( "+req.query.lat+" "+req.query.lng+" 2335 )' ), a.location, 200000) order by distance;", function(err, results) {
       if(err){
         res.status(500);
@@ -43,8 +53,8 @@ server.get('/api/closest-airport', function (req, res) {
     });
 
 });
-
-server.get('/api/flights', function (req, res) {
+*/
+/*server.get('/api/flights', function (req, res) {
   client.query("select distinct a.name, a.Country from flights as a inner join sourceStation as ss on a.source_airport_id=ss.id inner join destinationStation as ds on a.destination_airport_id = ds.id where ss.id="+req.query.id1+" and ds.id="+req.query.id2+";", function(err, results) {
     if(err){
         res.status(500);
@@ -57,7 +67,7 @@ server.get('/api/flights', function (req, res) {
   });
 
 });
-
+*/
 // Start server
 server.listen(3000, function () {
     console.log('Server up at 3000');
